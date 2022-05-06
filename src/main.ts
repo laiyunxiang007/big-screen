9ec20f6bbe0d86a5d203a8cb02b22a074ea2b2a2
laiyunxiang <294805447@qq.com>
2022-05-06 20:12:30 +0800

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import dataV from '@jiaminghi/data-view'
+
+createApp(App).use(store).use(router).mount('#app')
